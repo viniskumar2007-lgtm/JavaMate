99e7870d06c9b64945a6276931f2362f49080c6f
VINITH S <viniskumar2007@gmail.com>
2026-09-14 20:46:40 +0530

    Update javachat.py

--- javachat.py
+++ javachat.py
@@ -520,15 +520,15 @@ with st.sidebar:
 
 if "messages" not in st.session_state:
 
-st.session_state.messages = []
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
 
-avatar = (
-    "🧑‍💻"
-    if message["role"] == "user"
-    else "☕"
-)
+    avatar = (
+        "🧑‍💻"
+        if message["role"] == "user"
+        else "☕"
+    )
 
 
 with st.chat_message(
