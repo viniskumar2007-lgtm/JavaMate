a543abd1f392df21d418f3feef601cd9ebcd0015
VINITH S <viniskumar2007@gmail.com>
2026-09-14 20:16:23 +0530

    Update javachat.py

--- javachat.py
+++ javachat.py
@@ -523,7 +523,7 @@ RULES:
 "Sorry, I don't have that information
 in my Java knowledge base."
 """   
-            try:
+            
                     response = client.models.generate_content(
                         model=MODEL,
                         contents=prompt
