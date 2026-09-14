25f4e6edb1d3fa9239d76007e13b603b684a6250
VINITH S <viniskumar2007@gmail.com>
2026-09-14 21:08:11 +0530

    Update javachat.py

--- javachat.py
+++ javachat.py
@@ -519,6 +519,7 @@ with st.sidebar:
 #=========================================================
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
 for message in st.session_state.messages:
 
     role = message["role"]
@@ -531,15 +532,6 @@ for message in st.session_state.messages:
         avatar=avatar
     ):
         st.markdown(content)
-
-with st.chat_message(
-    message["role"],
-    avatar=avatar
-):
-
-    st.markdown(
-        message["content"]
-    )
 #=========================================================
 #USER INPUT
 #=========================================================
